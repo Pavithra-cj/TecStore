81b1d40d40fe5289c7a667d749dd47c3a6ebb5ed
Pavithra Chamod J <roguerevengerpcj2@gmail.com>
2024-07-11 18:31:20 +0530

    CRUD operations done.

---
+++ MobileStore/Services/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MobileStore.Models;
+
+namespace MobileStore.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
